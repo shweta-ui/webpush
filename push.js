0170fdc399d0dd2811eeefe7205c1f459e3b2758
Shweta Sinha <shweta_sinha@persistent.com>
2022-02-10 13:58:32 +0530

    change browser to chrome

--- push.js
+++ push.js
@@ -14,10 +14,11 @@ push.setVapidDetails('mailto:test@code.co.uk', vapidKeys.publicKey, vapidKeys.pr
 
 // sub is what we get from db, where pushed notification detail is stored
 let sub = {
-    "endpoint":"https://updates.push.services.mozilla.com/wpush/v2/gAAAAABiB…DeO2eaJ_NgsidLg6yX1ggErKJA_vkGQKikmWCZfs1HRdZDr2aWBxcbmV2PjQ",
+    "endpoint":"https://fcm.googleapis.com/fcm/send/c3lrU1_32fQ:APA91bHLNJ46fGHG759HI67uty9o1MExzjgTVNhxy4UuYSnn8pYuQWesQnE_YnRW3qzldbEU_2ivXdKlgYRO4F1Yo6jk9eCEinS5YFeQvcGH-F0DY4D8zyfav2ZCU_xtmviaBChsAefc",
+    "expirationTime":null,
     "keys":{
-        "auth":"BNpDjVHbBi8bthoYmipY0w",
-        "p256dh":"BLYJZ8lQFihnQmud7s8oHOJX3JnzzesEeoVv7ny7XayVymihUKVnGZoV0u4YcdVl25Dv6fxPRHilVHBVetKH_5c"
+        "p256dh":"BADzW85S9vAWQ0ePDnYJIlQulhmncZb_nbBX4FH0iYz4POisB88Wlg2JY1kL9o13P0_qJ0tzVk3kIhkHjNZ27eI",
+        "auth":"H6HagIBG0Z9lyrkgEAwpZg"
     }
 }
 
