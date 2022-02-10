af87109d763f50ffdfb5c959061245dd6e40bfd6
Shweta Sinha <shweta_sinha@persistent.com>
2022-02-10 13:48:03 +0530

    fix

--- push.js
+++ push.js
@@ -12,8 +12,14 @@ let vapidKeys = {
 
 push.setVapidDetails('mailto:test@code.co.uk', vapidKeys.publicKey, vapidKeys.privateKey)
 
-// sub is what we get from db where pushed notification detail is stored
-let sub = {};
+// sub is what we get from db, where pushed notification detail is stored
+let sub = {
+    "endpoint":"https://updates.push.services.mozilla.com/wpush/v2/gAAAAABiB…DeO2eaJ_NgsidLg6yX1ggErKJA_vkGQKikmWCZfs1HRdZDr2aWBxcbmV2PjQ",
+    "keys":{
+        "auth":"BNpDjVHbBi8bthoYmipY0w",
+        "p256dh":"BLYJZ8lQFihnQmud7s8oHOJX3JnzzesEeoVv7ny7XayVymihUKVnGZoV0u4YcdVl25Dv6fxPRHilVHBVetKH_5c"
+    }
+}
 
 push.sendNotification(sub, "test message")
 
